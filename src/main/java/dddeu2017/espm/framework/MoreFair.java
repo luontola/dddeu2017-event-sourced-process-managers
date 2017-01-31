4ae7366f70a22a3df7cfabdebc495d3a94f4c624
Esko Luontola <esko@luontola.fi>
2017-01-31 14:45:20 +0100

    MoreFair: prefer round-robin, instead of prefer first

--- src/main/java/dddeu2017/espm/framework/MoreFair.java
+++ src/main/java/dddeu2017/espm/framework/MoreFair.java
@@ -4,26 +4,26 @@ import dddeu2017.espm.HandlerOrder;
 import dddeu2017.espm.Order;
 import dddeu2017.espm.util.Util;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MoreFair implements HandlerOrder {
 
-    private final List<ThreadedHandler> handlers;
+    private final Queue<ThreadedHandler> handlers;
 
     public MoreFair(ThreadedHandler... handlers) {
-        this.handlers = new ArrayList<>(Arrays.asList(handlers));
+        this.handlers = new LinkedList<>(Arrays.asList(handlers));
     }
 
     @Override
     public void handle(Order order) {
         while (true) {
-            for (ThreadedHandler handler : handlers) {
-                if (handler.getCount() < 5) {
-                    handler.handle(order);
-                    return;
-                }
+            ThreadedHandler handler = handlers.peek();
+            handlers.add(handlers.remove());
+            if (handler.getCount() < 5) {
+                handler.handle(order);
+                return;
             }
             Util.sleep(1);
         }
