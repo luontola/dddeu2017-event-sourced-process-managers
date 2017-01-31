58da400268d34ecc3192b424d22b51743885fc29
Esko Luontola <esko@luontola.fi>
2017-01-31 10:58:12 +0100

    Place 10 orders

--- src/main/java/dddeu2017/espm/Application.java
+++ src/main/java/dddeu2017/espm/Application.java
@@ -1,12 +1,25 @@
 package dddeu2017.espm;
 
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
 public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        StopWatch stopWatch = StopWatch.createStarted();
         OrderPrinter printer = new OrderPrinter();
         Cashier cashier = new Cashier(printer);
         AssistantManager assistantManager = new AssistantManager(cashier);
         Cook cook = new Cook(assistantManager);
         Waiter waiter = new Waiter(cook);
-        waiter.placeOrder();
+        for (int i = 0; i < 10; i++) {
+            waiter.placeOrder();
+        }
+        log.info("Took {} seconds", stopWatch.getTime(TimeUnit.MILLISECONDS) / 1000.0);
     }
 }
