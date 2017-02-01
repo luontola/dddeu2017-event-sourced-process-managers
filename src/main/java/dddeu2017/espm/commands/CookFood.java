f15ba752eba8697458554f6212ef4a442f0e23bf
Esko Luontola <esko@luontola.fi>
2017-02-01 15:28:52 +0100

    Fix expiry on the correct message

--- src/main/java/dddeu2017/espm/commands/CookFood.java
+++ src/main/java/dddeu2017/espm/commands/CookFood.java
@@ -1,16 +1,23 @@
 package dddeu2017.espm.commands;
 
 import dddeu2017.espm.Order;
+import dddeu2017.espm.framework.Expirable;
 import dddeu2017.espm.framework.MessageBase;
 
+import java.time.Instant;
 import java.util.UUID;
 
-public class CookFood extends MessageBase {
+public class CookFood extends MessageBase implements Expirable {
 
     public final Order order;
 
     public CookFood(Order order, UUID correlationId, UUID causationId) {
         super(correlationId, causationId);
         this.order = order;
+    }
+
+    @Override
+    public Instant expires() {
+        return order.expires;
     }
 }
--- src/main/java/dddeu2017/espm/events/OrderPlaced.java
+++ src/main/java/dddeu2017/espm/events/OrderPlaced.java
@@ -1,23 +1,16 @@
 package dddeu2017.espm.events;
 
 import dddeu2017.espm.Order;
-import dddeu2017.espm.framework.Expirable;
 import dddeu2017.espm.framework.MessageBase;
 
-import java.time.Instant;
 import java.util.UUID;
 
-public class OrderPlaced extends MessageBase implements Expirable {
+public class OrderPlaced extends MessageBase {
 
     public final Order order;
 
     public OrderPlaced(Order order, UUID correlationId, UUID causationId) {
         super(correlationId, causationId);
         this.order = order;
-    }
-
-    @Override
-    public Instant expires() {
-        return order.expires;
     }
 }
